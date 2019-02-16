d1bbd12160609f75eb8c4b9f0d21e873e9be9d2d
XuanHung <hoxuanhunghust@gmail.com>
2019-02-16 16:16:37 +0700

    s
     á

--- js/init.js
+++ js/init.js
@@ -174,7 +174,7 @@ var eric = function (){
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ["^2100&nbsp;I Am Abe.<br/>UI / UX Developer.<br/> Living in New York City."],
+		strings: ["^2100&nbsp;I Am Xuan Hung.<br/>UI / UX Developer.<br/> Living in Ha Noi City."],
 		typeSpeed: 30,
 		backDelay: 750,
 		loop: false,
